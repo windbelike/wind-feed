20b93be85f43eec7e00262792a16e4d0c8b0c4de
sawyerrick <1103031078@qq.com>
2023-07-22 19:08:58 +0800

    feat: fix

--- src/components/NewThreadForm.tsx
+++ src/components/NewThreadForm.tsx
@@ -52,21 +52,6 @@ export default function NewThread() {
   )
 }
 
-type ProfileImgProps = {
-  className?: string
-  src?: string
-}
-
-function ProfileImg({ src, className = "" }: ProfileImgProps) {
-
-  return (
-    <div className="rounded-full h-12 w-12 overflow-hidden relative">
-      {src != null && <Image src={src} quality={100} alt="Profile Image" fill />}
-    </div>
-  )
-
-}
-
 type ButtonProps = {
   gray?: boolean
   small?: boolean
